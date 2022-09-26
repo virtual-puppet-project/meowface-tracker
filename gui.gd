e0ce084c7eaf7178a6cfbc7226fdc30f7b3a2064
Tim Yuen <timothy.yuen22@gmail.com>
2022-09-25 20:59:10 -0400

    fix bugs introduced when unflipping eyes, update translation, add inochi2d license because the tracking implementation used facetrack-d as a reference

--- gui.gd
+++ gui.gd
@@ -1,8 +1,22 @@
 extends PanelContainer
 
+const ConfigTrackerAddress: String = "MEOWFACE_TRACKER_ADDRESS"
+
 var logger := Logger.new("MeowFaceGUI")
 
+var regex: RegEx
+
 func _init() -> void:
+	var res := Safely.wrap(AM.cm.runtime_subscribe_to_signal(ConfigTrackerAddress))
+	if res.is_err():
+		logger.error(res)
+
+	regex = RegEx.new()
+	# https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp
+	if regex.compile("^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$") != OK:
+		logger.error("Unable to compile IPv4 regex, ip address checking will not work")
+		regex.compile("[\\s\\S]")
+
 	var sc := ScrollContainer.new()
 	ControlUtil.all_expand_fill(sc)
 
@@ -15,7 +29,14 @@ func _init() -> void:
 
 	vbox.add_child(_usage())
 
-	vbox.add_child(_toggle_tracking())
+	vbox.add_child(HSeparator.new())
+
+	var toggle_tracking: Button = _toggle_tracking()
+	vbox.add_child(_ip_address(toggle_tracking))
+
+	vbox.add_child(HSeparator.new())
+
+	vbox.add_child(toggle_tracking)
 
 func _usage() -> Label:
 	var r := Label.new()
@@ -25,6 +46,37 @@ func _usage() -> Label:
 	r.text = tr("MEOWFACE_USAGE_LABEL_TEXT")
 
 	return r
+
+func _ip_address(toggle: Button) -> HBoxContainer:
+	var r := HBoxContainer.new()
+	ControlUtil.h_expand_fill(r)
+	r.hint_tooltip = tr("MEOWFACE_IP_ADDRESS_HINT")
+
+	var label := Label.new()
+	ControlUtil.h_expand_fill(label)
+	label.text = tr("MEOWFACE_IP_ADDRESS_LABEL_TEXT")
+	label.hint_tooltip = tr("MEOWFACE_IP_ADDRESS_HINT")
+
+	r.add_child(label)
+
+	var line_edit := LineEdit.new()
+	ControlUtil.h_expand_fill(line_edit)
+	line_edit.text = AM.cm.get_data(ConfigTrackerAddress, "*")
+	line_edit.hint_tooltip = tr("MEOWFACE_IP_ADDRESS_HINT")
+
+	line_edit.connect("text_changed", self, "_on_ip_address_changed", [toggle])
+
+	r.add_child(line_edit)
+
+	return r
+
+func _on_ip_address_changed(text: String, toggle: Button) -> void:
+	if regex.search(text) == null:
+		toggle.disabled = true
+		return
+	else:
+		toggle.disabled = false
+		AM.ps.publish(ConfigTrackerAddress, text)
 
 func _toggle_tracking() -> Button:
 	var r := Button.new()
--- tracker.gd
+++ tracker.gd
@@ -1,5 +1,7 @@
 extends TrackingBackendTrait
 
+const ConfigTrackerAddress: String = "MEOWFACE_TRACKER_ADDRESS"
+
 const EMPTY_VEC3_DICT := {"x": 0.0, "y": 0.0, "z": 0.0}
 
 var tracking_data := {}
@@ -34,15 +36,14 @@ func _perform_reception() -> void:
 		OS.delay_msec(server_poll_interval)
 
 func _receive() -> void:
-	if client.put_packet(JSON.print({
-				"messsageType": "iOSTrackingDataRequest", # HMMMM
-				"time": 1.0,
-				"sentBy": "vpuppr",
-				"ports": [
-					21412
-				]
-			}).to_utf8()) != OK:
-		logger.error("Unable to send packet")
+	client.put_packet(JSON.print({
+		"messsageType": "iOSTrackingDataRequest", # HMMMM
+		"time": 1.0,
+		"sentBy": "vpuppr",
+		"ports": [
+			21412
+		]
+	}).to_utf8())
 	
 	if client.get_available_packet_count() < 1:
 		return
@@ -67,7 +68,7 @@ func start_receiver() -> void:
 
 	logger.info("Starting receiver")
 
-	var address: String = "192.168.88.229"
+	var address: String = AM.cm.get_data(ConfigTrackerAddress)
 	var port: int = 21412
 	
 	client = PacketPeerUDP.new()
@@ -87,7 +88,6 @@ func stop_receiver() -> void:
 	logger.info("Stopping face tracker")
 
 	stop_reception = true
-	
 
 	if receive_thread != null and receive_thread.is_active():
 		receive_thread.wait_to_finish()
@@ -109,20 +109,20 @@ func apply(interpolation_data: InterpolationData, model: PuppetTrait) -> void:
 	
 	var tx: Dictionary = tracking_data.get("Position", EMPTY_VEC3_DICT)
 	var rx: Dictionary = tracking_data.get("Rotation", EMPTY_VEC3_DICT)
-	var eye_l: Dictionary = tracking_data.get("EyeLeft", EMPTY_VEC3_DICT)
-	var eye_r: Dictionary = tracking_data.get("EyeRight", EMPTY_VEC3_DICT)
 	
 	interpolation_data.bone_translation.target_value = Vector3(-tx.y, -tx.x, tx.z)
 	interpolation_data.bone_rotation.target_value = Vector3(-rx.y, -rx.x, rx.z)
-	interpolation_data.left_gaze.target_value = Vector3(eye_l.y, eye_l.x, eye_l.z) / 100.0
-	interpolation_data.right_gaze.target_value = Vector3(eye_r.y, eye_r.x, eye_r.z) / 100.0
+	interpolation_data.left_gaze.target_value = JSONUtil.dict_to_vector3(
+		tracking_data.get("EyeRight", EMPTY_VEC3_DICT)) / 100.0
+	interpolation_data.right_gaze.target_value = JSONUtil.dict_to_vector3(
+		tracking_data.get("EyeLeft", EMPTY_VEC3_DICT)) / 100.0
 	
 	for data in tracking_data.get("BlendShapes", []):
 		match data.k:
 			"eyeBlinkLeft":
-				interpolation_data.left_blink.target_value = 1.0 - data.v
-			"eyeBlinkRight":
 				interpolation_data.right_blink.target_value = 1.0 - data.v
+			"eyeBlinkRight":
+				interpolation_data.left_blink.target_value = 1.0 - data.v
 			_:
 				for mesh_instance in model.skeleton.get_children():
 					mesh_instance.set("blend_shapes/%s" % data.k, data.v)
